7c1f64e609729680eb721e25cc60acaadcffc8f7
Alexander Shirk <alexander@truewheels.net>
2022-02-15 14:08:19 -0800

    added empty functions for all commands

---
+++ src/commands.h
@@ -0,0 +1,71 @@
+/*
+This file contains the functions for all the commands
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+// int get() {
+//     printf("get\n");
+//     return 0;
+// }
+int get() {
+    printf("get\n");
+    return 0;
+}
+int put() {
+    printf("put\n");
+    return 0;
+}
+int ls() {
+    printf("ls\n");
+    return 0;
+}
+int cd() {
+    printf("cd\n");
+    return 0;
+}
+int pwd() {
+    printf("pwd\n");
+    return 0;
+}
+int mymkdir() {
+    printf("mymkdir\n");
+    return 0;
+}
+int myrmdir() {
+    printf("myrmdir\n");
+    return 0;
+}
+int rm() {
+    printf("rm\n");
+    return 0;
+}
+int lcat() {
+    printf("lcat\n");
+    return 0;
+}
+int lls() {
+    printf("lls\n");
+    return 0;
+}
+int lcd() {
+    printf("lcd\n");
+    return 0;
+}
+int lpwd() {
+    printf("lpwd\n");
+    return 0;
+}
+int lmkdir() {
+    printf("lmkdir\n");
+    return 0;
+}
+int lrmdir() {
+    printf("lrmdir\n");
+    return 0;
+}
+int lrm() {
+    printf("lrm\n");
+    return 0;
+}
