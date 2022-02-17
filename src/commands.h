f9ee9decb0a8a0f9db78663c55a175e28354a045
Alexander Shirk <alexander@truewheels.net>
2022-02-16 21:43:21 -0800

    ls code from book

--- src/commands.h
+++ src/commands.h
@@ -5,6 +5,12 @@ This file contains the functions for all the commands
 #include <stdio.h>
 #include <string.h>
 
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <time.h>
+#include <sys/types.h>
+#include <dirent.h>
+
 char printHolder[2000];
 
 // int get() {
@@ -49,18 +55,48 @@ int lcat(char* filename) {
 }
 int lls(char* filename) {
     sprintf(printHolder, "lls, filename=%s\n", filename);
-    return 0;
-}
-int lcd(char* filename) {
-    sprintf(printHolder, "lcd, filename=%s\n", filename);
-    return 0;
-}
-int lpwd(char* filename) {
-    sprintf(printHolder, "lpwd, filename=%s\n", filename);
-    return 0;
-}
-int lmkdir(char* filename) {
-    sprintf(printHolder, "lmkdir, filename=%s\n", filename);
+    sprintf(printHolder, "test");
+    struct stat fstat, *sp;
+    char *t1 = "xwrxwrxwr-------";
+    char *t2 = "----------------";
+    int r, i; 
+    char ftime[64]; 
+    sp = &fstat;
+    if ((r = lstat(filename, &fstat)) < 0) {
+        printf("can't stat %s\n", filename); 
+        exit(1);
+    }
+    if ((sp->st_mode & 0xF000) == 0x8000) { //if (S_ISREG())
+        printf("%c", '-');
+    }
+    if ((sp->st_mode & 0xF000) == 0x4000) { //if (S_ISDIR())
+        printf("%c", 'd');
+    }
+    if ((sp->st_mode & 0xF000) == 0xA000) { //if (S_ISLNK())
+        printf("%c", 'l');
+    }
+    for (int i = 8; i >= 0; i--) {
+        if (sp->st_mode & (1 << i)) { 
+            printf("%c", t1[i]); //print r | w | x
+        } else { 
+            printf("%c", t2[i]); //or print -
+        }
+    }
+    printf("%4d ", sp->st_nlink); //link count 
+    printf("%4d ", sp->st_gid); //gid 
+    printf("%4d ", sp->st_uid); //uid 
+    printf("%8d ", sp->st_size); //file size 
+    //print time 
+    printf("%s", basename(filename)); //print file basename 
+    //print -> linkname if symbolic file 
+    // if ((sp->st_mode & 0xF000) == 0xA000) {
+    //     //use readlink() to read linkname 
+    //     printf(" -> %s", readlink(linkname));
+    // }
+    printf("\n");
+
+    
+
     return 0;
 }
 int lrmdir(char* filename) {
@@ -70,4 +106,13 @@ int lrmdir(char* filename) {
 int lrm(char* filename) {
     sprintf(printHolder, "lrm, filename=%s\n", filename);
     return 0;
+}
+int lcd(char* filename) {
+    sprintf(printHolder, "lrm, filename=%s\n", filename);
+}
+int lpwd(char* filename) {
+    sprintf(printHolder, "lrm, filename=%s\n", filename);
+}
+int lmkdir(char* filename) {
+    sprintf(printHolder, "lrm, filename=%s\n", filename);
 }
